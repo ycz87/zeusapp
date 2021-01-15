a9bf3fd0c47f43fee534094c66a27cc568d52ebb
ycz87 <yuchangzhou@gmail.com>
2021-01-15 23:13:04 +0800

    add prolayout

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+
+import type { MenuDataItem } from '@ant-design/pro-layout';
+import ProLayout, { PageContainer } from '@ant-design/pro-layout';
+import { Button } from 'antd';
+
+import './App.css';
+import '@ant-design/pro-layout/dist/layout.css';
+import customMenuDate from './customMenu';
+export default () => {
+  const [menuData, setMenuData] = useState<MenuDataItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    setMenuData([]);
+    setLoading(true);
+    setTimeout(() => {
+      setMenuData(customMenuDate);
+      setLoading(false);
+    }, 2000);
+  }, [index]);
+  return (
+    <>
+      <Button
+        onClick={() => setIndex(index + 1)}
+        style={{
+          margin: 8,
+        }}
+      >
+        重新加载
+      </Button>
+      <ProLayout
+        style={{
+          height: '100vh',
+          border: '1px solid #ddd',
+        }}
+        menu={{
+          loading,
+        }}
+        location={{
+          pathname: '/welcome/welcome',
+        }}
+        menuDataRender={() => menuData}
+      >
+        <PageContainer content="欢迎使用">Hello World</PageContainer>
+      </ProLayout>
+    </>
+  );
+};
