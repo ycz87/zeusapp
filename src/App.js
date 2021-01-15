99ce8552e6a805a568e31dd8854b0b6305e0283e
ycz87 <yuchangzhou@gmail.com>
2021-01-15 22:03:35 +0800

    Revert "add antd"
    This reverts commit 98338e25a2bfa48d53580a3b0b4cbd7358988770.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Button } from 'antd';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
